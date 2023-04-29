000e16a17b769ad6161d866dae921855427275cf
PGS <sarangipriyanshu@gmail.com>
2023-04-29 22:39:14 +0530

    working on it slowly

---
+++ Noob_Coderz/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+export default function App() {
+  return (
+    <section>
+      <h2> WELCOME TO NOOB CODERZ </h2>
+      <div>
+        <p>Currently Around Construction</p>
+
+      </div>
+    </section>
+
+  )
+}
