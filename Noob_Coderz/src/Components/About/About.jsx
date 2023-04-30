758deafc9b2b1f9507cbb5293ff44cd65b0bc1ea
PGS <sarangipriyanshu@gmail.com>
2023-04-30 13:20:04 +0530

    working 2.0

---
+++ Noob_Coderz/src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+     <section>
+        <h2>WELCOME TO NOOB CODERZ</h2>
+        <div className='container about_content'>
+            <p>
+            We are a  team of enthusiastic coders who are constantly learning  in the world of software development. As coders collaborate and share their knowledge with one another, they create a community of like-minded individuals who are passionate about creating innovative solutions to complex problems. Through their dedication to learning, this team of coders will continue to push the boundaries of what is possible in the world of technology, inspiring others  to make a positive impact on the world.
+            </p>
+        </div>
+     </section>
+  )
+}
