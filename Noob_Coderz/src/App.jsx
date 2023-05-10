0fe7e1a83b6423adf52250dddd56116b6f6cf42f
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-05-10 17:19:32 +0530

    For Jd

--- Noob_Coderz/src/App.jsx
+++ Noob_Coderz/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './App.css'
 import About from './Components/About/About'
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
 export default function App() {
   return (
     <div>
